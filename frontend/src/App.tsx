3496ad93de31633769ae7a40a1dc301dce226c9b
Bruno Mendola <1853562+brunomendola@users.noreply.github.com>
2025-04-17 12:03:13 +0200

    Update @queritylib/react to version 1.3.0
    Removed custom styles to use the default theme from @queritylib/react

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,17 +5,6 @@ import './App.css'
 
 const querityComponents: ComponentOverrides = {
   ...defaultQuerityComponents,
-  Input: (props) => <input {...props} className={`${props.className} border p-2`} />,
-  Select: (props) => <select {...props} className={`${props.className} border p-2`} />,
-  Button: (props) => <button {...props} className={`${props.className} bg-gray-200 p-2 mr-1 ml-1 cursor-pointer`} />,
-  Checkbox: (props) => (
-      <label className="inline-flex items-center cursor-pointer">
-        <input type="checkbox" {...props} className="sr-only peer"/>
-        <div
-            className="relative w-11 h-6 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full rtl:peer-checked:after:-translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:start-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600 dark:peer-checked:bg-blue-600"></div>
-        <span className="ms-3 text-sm font-medium text-gray-900 dark:text-gray-300">{props.label}</span>
-      </label>
-  )
 };
 
 function App() {
